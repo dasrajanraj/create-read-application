fd55f77e190702c6f87bef03640819da95f85fec
Rajan Raj Das <dasrajanraj@gmail.com>
2020-03-27 18:00:31 +0530

    Front End Ready

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ app.set("views","view")
 
 app.use(express.static(__dirname+'/public'));
 
+var list = [{ name: 'Rajan',email: 'rajan.beis.16@acharya.ac.in',age: '12',mobile: '6361500245',address: '#6,7th Cross, Masjid Road, Soldevanahalli',state: 'KA' }];
+
 app.get("/",(req,res,next)=>{
-    res.render('index');
-    console.log("This is testing page");
+    res.render('index',{list:list});
+})
+app.post("/",(req,res,next)=>{
+    const obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    list.push(obj);
+    res.redirect('/');
 })
 
 const port = process.env.PORT || 3000;
